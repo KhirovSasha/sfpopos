68c310d34e46cc0ffbd4a3efcf3cbd27eebca066
Khiro Sasha <sasha.khiro@gmail.com>
2022-12-03 02:11:49 +0200

    dynamic content and footer

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./POPOSSpace.css";  
 
 function POPOSSpace(props){
-    const {name, image, address, likes} = props
+    const {name, image, address, likes, hours} = props
     return(
         <div className="POPOSSpace">
             <img src = {image}
@@ -12,6 +12,7 @@ function POPOSSpace(props){
             />
             <h1>{name}</h1>
             <div>{address}</div>
+            <div className="hours">{hours}</div>
             <div className="Likes">
                 Likes: {likes}
                 <img src="https://img.icons8.com/material-outlined/512/facebook-like.png" />
