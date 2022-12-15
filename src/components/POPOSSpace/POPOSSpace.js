b74450c6956360c657cbd5d5cb4c4ca842094ad8
Khiro Sasha <sasha.khiro@gmail.com>
2022-12-15 21:03:56 +0200

    Styling Spaces component

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -14,18 +14,21 @@ function POPOSSpace(props){
                 />
                 
             </Link>
-            
-            <h1>
-                <Link to={`/details/${id}`}>
-                    {name} 
-                </Link>
-            </h1>
-            <div>{address}</div>
-            <div className="hours">{hours}</div>
-            <div className="Likes">
-                Likes: {likes}
-                <img src="https://img.icons8.com/material-outlined/512/facebook-like.png" alt={name}/>
+
+            <Link
+                className="POPOSSpace-title" 
+                to={`/details/${id}`}>
+                   <h1>{name}</h1> 
+            </Link>
+            <div className="POPOSSpace-info">
+                <div>{address}</div>
+                <div className="hours">{hours}</div>
+                <div className="Likes">
+                    Likes: {likes}
+                    <img src="https://img.icons8.com/material-outlined/512/facebook-like.png" alt={name}/>
             </div>
+            </div>
+            
         </div>
     )
 }
