428202a79c3a9f413bb61f4293366fc9e6c6b5eb
Khiro Sasha <sasha.khiro@gmail.com>
2022-12-17 19:59:53 +0200

    Updates

---
+++ precache-manifest.6244117d8fbb98e6aa9d2b3b2f3fea80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67770fa6420c80021350",
+    "url": "/sfpopos/static/css/main.f7c6bf69.chunk.css"
+  },
+  {
+    "revision": "67770fa6420c80021350",
+    "url": "/sfpopos/static/js/main.67770fa6.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "/sfpopos/static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "128a68b680e26aa74264",
+    "url": "/sfpopos/static/js/2.128a68b6.chunk.js"
+  },
+  {
+    "revision": "ef51c51a9a04b8d72c50",
+    "url": "/sfpopos/static/js/runtime~main.ef51c51a.js"
+  },
+  {
+    "revision": "2d19448b6dffb2fee665fbae0f16cea4",
+    "url": "/sfpopos/index.html"
+  }
+];
